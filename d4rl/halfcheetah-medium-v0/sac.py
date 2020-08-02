6eb9e5aa5f733230b62fa65d30f69fd5734a49ec
cheneyuwu <cheneyuwu@gmail.com>
2020-08-01 22:07:18 -0400

    gan exp

--- d4rl/halfcheetah-medium-v0/sac.py
+++ d4rl/halfcheetah-medium-v0/sac.py
@@ -12,8 +12,12 @@ params_config["seed"] = tuple(range(5))
 # Exp 1 sac.
 params_config["config"] = ("SAC", )
 
-# Exp 2 copy actor, critic and alpha from a pretrained cql agent: cql.pkl
-params_config["config"] = ("SAC_CQLPretrained", )
-params_config["agent"]["use_pretrained_critic"] = True
-params_config["agent"]["use_pretrained_actor"] = True
-params_config["agent"]["use_pretrained_alpha"] = True
+# # Exp 2 copy actor, critic and alpha from a pretrained cql agent: cql.pkl
+# params_config["config"] = ("SAC_CQLPretrained", )
+# params_config["agent"]["use_pretrained_critic"] = True
+# params_config["agent"]["use_pretrained_actor"] = True
+# params_config["agent"]["use_pretrained_alpha"] = True
+
+# # Exp 3 copy actor, critic and alpha from a pretrained gan agent: gan.pkl
+# params_config["config"] = ("SAC_GANPretrained", )
+# params_config["agent"]["use_pretrained_critic"] = True
