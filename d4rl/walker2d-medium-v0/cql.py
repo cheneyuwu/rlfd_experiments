ad46fcbf28becda0cd40f7acc5720133636d4a78
Yuchen Wu <yuchenwu@beluga2.int.ets1.calculquebec.ca>
2020-08-05 08:02:19 -0400

    add hopper/walker2d params

---
+++ d4rl/walker2d-medium-v0/cql.py
@@ -0,0 +1,10 @@
+from copy import deepcopy
+from rlfd.params.cql import gym_mujoco_params
+
+params_config = deepcopy(gym_mujoco_params)
+
+params_config["config"] = ("CQL",)
+
+params_config["env_name"] = "walker2d-medium-v0"
+# Tuned values
+params_config["seed"] = tuple(range(5))
---
+++ d4rl/walker2d-medium-v0/nf.py
@@ -0,0 +1,13 @@
+from copy import deepcopy
+from rlfd.params.nf import gym_mujoco_params
+
+params_config = deepcopy(gym_mujoco_params)
+
+params_config["config"] = ("NF", )
+
+params_config["env_name"] = "walker2d-medium-v0"
+# Tuned values
+params_config["seed"] = tuple(range(5))
+
+# Exp 1 NF
+params_config["config"] = ("NF", )
