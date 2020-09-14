d7beac944b9796b9a125afce24893eef77d36418
Yuchen Wu <cheneyuwu@gmail.com>
2020-09-13 23:19:56 -0400

    sac offline params

---
+++ d4rl/walker2d-medium-v0/sac_offline.py
@@ -0,0 +1,15 @@
+from copy import deepcopy
+from rlfd.params.sac_offline import gym_mujoco_params
+
+params_config = deepcopy(gym_mujoco_params)
+
+params_config["config"] = ("SACOffline", )
+
+params_config["env_name"] = "walker2d-medium-v0"
+# Tuned values
+params_config["seed"] = tuple(range(5))
+
+# Exp 1 sac offline.
+params_config["config"] = ("SACOffline", )
+params_config["agent"]["online_sample_ratio"] = 0.5
+params_config["seed"] = tuple(range(5))
